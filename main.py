2ea8be65d8151979f5d720152da856f2833781eb
Dima-Gomel <metalland2015@yandex.ru>
2025-02-05 23:14:49 +0300

    Task2 - Бот

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 from telebot import TeleBot, types
 import random
-import config
+import config, messages, jokes
 
 bot = TeleBot(config.BOT_TOKEN)
-
-help_message = """Привет! Доступные команды:
-- /start - начало работы с ботом
-- /help - помощь (это сообщение)
-- /joke - случайная шутка
-
-Этот бот отправит вам тоже сообщение, что и вы ему.
-"""
-
-KNOWN_JOKES = [
-    'В 47–ой книге Гарри Поттер убивает Джоан Роулинг.',
-    'В пиве мало витаминов – вот почему его приходится пить так много.',
-    'Новая секретарша английского не знала, но языком владела в совершенстве…'
-]
 
 
 @bot.message_handler(commands=["start"])
 def handle_command_start(message: types.Message):
     bot.send_message(
         message.chat.id,
-        'Привет! Давай знакомиться!'
+        messages.START_MESSAGE
     )
 
 
@@ -31,7 +17,7 @@ def handle_command_start(message: types.Message):
 def handle_command_help(message: types.Message):
     bot.send_message(
         message.chat.id,
-        help_message
+        messages.HELP_MESSAGE
     )
 
 
@@ -39,14 +25,19 @@ def handle_command_help(message: types.Message):
 def handle_command_joke(message: types.Message):
     bot.send_message(
         message.chat.id,
-        random.choice(KNOWN_JOKES)
+        random.choice(jokes.KNOWN_JOKES)
     )
+
 
 @bot.message_handler()
 def echo_message(message: types.Message):
     text = message.text
     if 'привет' in text.lower():
-        text = 'Привет! Как дела?'
+        text = 'И тебе привет!'
+    elif 'как дела' in text.lower():
+        text = 'Хорошо! А у вас как?'
+    elif 'пока' or 'до свидания' in text.lower():
+        text = 'До новых встреч!'
     bot.send_message(
     message.chat.id,
     text
