3e68ce3fcaead5c61c9ab0ee7ab5cb15563b1516
Dima-Gomel <metalland2015@yandex.ru>
2025-02-07 21:53:21 +0300

    Task3 - Бот с обработкой фото

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from telebot import TeleBot, types
 import random
-import config, messages, jokes
+
+import config
+import messages
+import jokes
 
 bot = TeleBot(config.BOT_TOKEN)
 
@@ -29,19 +32,63 @@ def handle_command_joke(message: types.Message):
     )
 
 
-@bot.message_handler()
+@bot.message_handler(commands=['dogs'])
+def send_dogs_photo(message: types.Message):
+    bot.send_photo(
+        chat_id=message.chat.id,
+        photo=config.DOG_PIC_URL
+    )
+
+
+def is_cat_in_caption(message: types.Message):
+    return message.caption and 'кот' in message.caption.lower()
+
+
+@bot.message_handler(content_types=['photo'], func=is_cat_in_caption)
+def handle_photo_with_caption(message: types.Message):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='Какой классный кот!'
+    )
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message: types.Message):
+    if message.caption:
+        print('Подпись: ', message.caption)
+    else:
+        print('Отсутствует подпись: ', message.caption)
+    photo_file_id = message.photo[-1].file_id
+    bot.send_photo(
+        chat_id=message.chat.id,
+        photo=photo_file_id,
+        reply_to_message_id=message.id
+    )
+
+
+@bot.message_handler(content_types=['sticker'])
+def handle_sticer(message: types.Message):
+    bot.send_sticker(
+        chat_id=message.chat.id,
+        sticker=message.sticker.file_id,
+        reply_to_message_id=message.id
+    )
+
+
+@bot.message_handler(content_types=['text'])
 def echo_message(message: types.Message):
     text = message.text
     if 'привет' in text.lower():
         text = 'И тебе привет!'
     elif 'как дела' in text.lower():
         text = 'Хорошо! А у вас как?'
-    elif 'пока' or 'до свидания' in text.lower():
+    elif ('пока' or 'до свидания') in text.lower():
         text = 'До новых встреч!'
     bot.send_message(
-    message.chat.id,
-    text
+        chat_id=message.chat.id,
+        text=text
     )
 
 
-bot.infinity_polling(skip_pending=True)
+if __name__ == '__main__':
+    bot.infinity_polling(skip_pending=True)
